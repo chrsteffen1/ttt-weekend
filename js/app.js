438f22c1a7aec2485b4cdf436895b926af9a146f
Chrsteffen <Chrsteffen1@gmail.com>
2022-09-01 17:32:09 -0400

    create cached elements

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const squareEl=document.querySelector('.board-square')
+const messageEl = document.querySelector('#message')
+console.log(squareEl)
+console.log(messageEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
