c9576a242e6befff65265da2dbb3f48f4c387e9c
Chrsteffen <Chrsteffen1@gmail.com>
2022-09-05 18:22:19 -0400

    add animation

--- js/app.js
+++ js/app.js
@@ -9,7 +9,12 @@ let board, turn, winner, sqIdx, totalTurns
 let squareEl = document.querySelectorAll('.board-square')
 let messageEl = document.querySelector('#message')
 let boardSquare = document.querySelector('section.board')
-const resetBtn = document.querySelector("#reset-button")
+let resetBtn = document.querySelector('#reset-button')
+let boxEL = document.querySelector('section')
+let backC = document.querySelector('body')
+let titleEl = document.querySelector('h1')
+let winnerEmoji = document.querySelector('#winner')
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 boardSquare.addEventListener('click', handleClick)
@@ -24,30 +29,38 @@ function init() {
   winner = null
   totalTurns = 0
   resetBtn.setAttribute("hidden", true)
+  messageEl.className = ''
+  backC.className = ''
+  titleEl.className = ''
   render()
 }
 
 function render() {
   board.forEach((squ, idx) => {
     if (squ === 1) {
-      squareEl[idx].textContent = 'X'
+      squareEl[idx].textContent = '👽';
+      squareEl.className ='animate__animated animate__zoomInDown' 
     } 
     if (squ === -1) {
-      squareEl[idx].textContent = 'O'
+      squareEl[idx].textContent = '🛸'
     }
     if (squ === null) {
       squareEl[idx].textContent = ""
     }
     })
-  if (winner === null) {
-    messageEl.textContent = `It's player ${turn}'s turn`
-  } 
-  if (winner === 'T') {
-  messageEl.textContent = 'You both tied'}
-  if (totalTurns !== 0){
-    resetBtn.removeAttribute('hidden')
-  }
+    if (winner === null) {
+      messageEl.textContent = `${turn === 1 ? "👽" : "🛸"}! It's your turn`
+    } 
+    if (winner === 'T') {
+      messageEl.textContent = 'You both tied'}
+      if (totalTurns !== 0){
+        resetBtn.removeAttribute('hidden')
+      }
+  
 }
+
+
+
 function handleClick(evt) {
   sqIdx = parseInt(evt.target.id.replace('sq', ''))
     if (board[sqIdx] !== null || winner !== null){
@@ -59,20 +72,33 @@ function handleClick(evt) {
     totalTurns += 1
     
     getWinner()
-    render()
-    
+    render()  
+    winnerBoard() 
 }
 
 function getWinner() {
   if (board[0] + board[1] + board[2] === 3 || board[3] + board[4] + board[5] === 3 || board[6] + board[7] + board[8] === 3 || board[0] + board[3] + board[6] === 3 || board[1] + board[4] + board[7] === 3 || board[2] + board[5] + board[8] === 3 || board[0] + board[4] + board[8] === 3 || board[2] + board[4] + board[6] === 3){
-    messageEl.textContent = "X wins our highest honor"
+    messageEl.textContent = "👽 wins our highest honor"
+    winnerEmoji.textContent = "👽"
     winner = true
   }
   if (board[0] + board[1] + board[2] === -3 || board[3] + board[4] + board[5] === -3 || board[6] + board[7] + board[8] === -3 || board[0] + board[3] + board[6] === -3 || board[1] + board[4] + board[7] === -3 || board[2] + board[5] + board[8] === -3 || board[0] + board[4] + board[8] === -3 || board[2] + board[4] + board[6] === -3){
-    messageEl.textContent = "O wins our highest honor"
+    messageEl.textContent = "🛸 wins our highest honor"
+    winnerEmoji.textContent = "🛸"
     winner = true
   }
   if (totalTurns === 9 && winner === null){
     winner = 'T'
   }
+}
+
+function winnerBoard() {
+  if (winner === true){
+    messageEl.className = 'animate__animated animate__flash animate__infinite'
+    backC.className = 'animate-animated animate__pulse animate__duration-5s' 
+    titleEl.className = 'animate__animated animate__heartBeat animate__infinite'
+    boxEL.style.visibility = 'hidden'
+    winnerEmoji.className = 'animate__animated animate__bounceOutUp animate__slow '
+  } 
+
 }
